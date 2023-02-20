3024eda4c8e2428c205b7b8f9293dcf9cf85a2c1
AlessandraSantos-dev <allems7@gmail.com>
2023-02-20 18:27:45 +0000

    Added Bootstraap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
+import "./Weather.css";
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/AlessandraSantos-dev/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alessandra Santos
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
