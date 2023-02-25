627bb5c07f12c5ce51c08d72929e92036942e34f
AlessandraSantos-dev <allems7@gmail.com>
2023-02-25 14:53:51 +0000

    Creation of WeatherTemperature component and added temperature in ºF

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">ºC</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function displayFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function displayCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          ºC |{" "}
+          <a href="/" onClick={displayFahrenheit}>
+            {" "}
+            ºF{" "}
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={displayCelsius}>
+            ºC
+          </a>
+          | ºF
+        </span>
+      </div>
+    );
+  }
 }
